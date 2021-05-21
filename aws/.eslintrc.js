a07d92611e577a038d4b5146ba07ed1a7c313e95
Chris-Robin Ennen <ce053@hdm-stuttgart.de>
2021-05-21 13:27:07 +0200

    add standard config

--- aws/.eslintrc.js
+++ aws/.eslintrc.js
@@ -2,21 +2,23 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    "jest/globals": true
+    jest: true,
   },
   overrides: [
     {
-      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      files: ['*.ts', '*.tsx'],
       parserOptions: {
         tsconfigRootDir: __dirname,
-        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+        project: ['./tsconfig.json'],
       },
     },
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'standard', 'prettier'],
   rules: {
-    "@typescript-eslint/no-explicit-any": "off"
-  }
-};
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    semi: 'warn',
+  },
+}
