59c1beb5b6dc062cdcb8673f28d5c426bdc7fd62
Chris-Robin Ennen <ce053@hdm-stuttgart.de>
2021-05-22 22:27:01 +0200

    add comments to aws client

--- aws/src/awsClient.ts
+++ aws/src/awsClient.ts
@@ -20,6 +20,9 @@ export type Message = {
   desired: Record<string, any>
 }
 
+/**
+ *  A client to connect to AWS IoT on a single topic.
+ */
 export class AwsClient {
   protected connection: mqtt.MqttClientConnection
   protected decoder = new TextDecoder('utf8')
@@ -37,6 +40,14 @@ export class AwsClient {
     this.connection = this.getMqttConnection(config)
   }
 
+  /**
+   * Builds a config for an AWS connection.
+   *
+   * @remarks Pretty basic, mostly taken from the AWS sdk sample.
+   *
+   * @param argv - An object containing configuration options.
+   * @returns A config object.
+   */
   private buildConfig(argv: Args) {
     const builder = iot.AwsIotMqttConnectionConfigBuilder.new_mtls_builder_from_path(argv.cert, argv.key)
 
@@ -49,23 +60,40 @@ export class AwsClient {
     return builder.build()
   }
 
+  /**
+   *
+   * @param config - A config object
+   * @returns An AWS connection that is not connected yet.
+   */
   private getMqttConnection(config: mqtt.MqttConnectionConfig) {
     const bootstrap = new io.ClientBootstrap()
     const client = new mqtt.MqttClient(bootstrap)
     return client.new_connection(config)
   }
 
-  connect() {
-    return this.connection.connect()
+  /**
+   * Connect to AWS IoT. Call this before trying to subscribe or publish.
+   */
+  async connect() {
+    await this.connection.connect()
   }
 
-  disconnect() {
-    return this.connection.disconnect()
+  /**
+   * Disconnect from AWS IoT.
+   */
+  async disconnect() {
+    await this.connection.disconnect()
   }
 
-  subscribe(callback: (message: Message) => void) {
+  /**
+   * Subscribe to the thing this client was created for.
+   * Call {@link connect} before trying to subscribe.
+   * @param callback - The callback to invoke whenever a message is received.
+   * @returns Promise which resolves when the subscription succeeds and rejects if it fails.
+   */
+  async subscribe(callback: (message: Message) => void) {
     console.log('subscribing to ', this.topic)
-    return this.connection.subscribe(this.topic, mqtt.QoS.AtLeastOnce, (_, payload) => {
+    await this.connection.subscribe(this.topic, mqtt.QoS.AtLeastOnce, (_, payload) => {
       const decoded = this.decoder.decode(payload)
       const json = JSON.parse(decoded)
       const msg: Message = json.state
@@ -73,9 +101,15 @@ export class AwsClient {
     })
   }
 
-  publish(reported: Message['reported']) {
+  /**
+   * Publish a payload to this client's topic.
+   * Call {@link connect} before trying to publish.
+   * @param reported - The state to be published.
+   * @returns Promise that resolves when the message has been published.
+   */
+  async publish(reported: Message['reported']) {
     const payload = { state: { reported } }
     const data = JSON.stringify(payload)
-    return this.connection.publish(this.topic, data, mqtt.QoS.AtLeastOnce)
+    await this.connection.publish(this.topic, data, mqtt.QoS.AtLeastOnce)
   }
 }
